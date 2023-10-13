d5f409f1a0f2d1d5319a350c0ea6d29388d5c4c1
hugo-michel <orayito62@gmail.com>
2023-10-13 14:13:21 +0200

    merge faq + tag

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -7,7 +7,6 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Faq;
 
 #[Route('/test')]
