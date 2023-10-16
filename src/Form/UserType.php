f63e84d334f4c148144b9ec23af1b28a42f2896d
Sego <s.henniart.webdev@gmail.com>
2023-10-16 21:05:05 +0200

    add: EditeurType->addEmail user.email dans template editeur

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -46,15 +46,16 @@ class UserType extends AbstractType
                 'required' => true
             ])
             // Ajout d'un event listener pour lancer le hasher au submit
-            ->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) use ($hasher) {
-                $user = $event->getData();
-                $password = $user->getPassword();
-                $hashedPassword = $hasher->hashPassword($user, $password);
-                $user->setPassword($hashedPassword);
-                $event->setData($user);
-            })
+            // ->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) use ($hasher) {
+            //     $user = $event->getData();
+            //     $password = $user->getPassword();
+            //     $hashedPassword = $hasher->hashPassword($user, $password);
+            //     $user->setPassword($hashedPassword);
+            //     $event->setData($user);
+            // })
             //! Le MDP est modifié par le hasher, si un user veut modifier 
             //! son MDP, celui qu'il a créé n'apparaît pas dans le champ MDP
+            //& il faut rendre accessible le MDP non hashé pour l'utilisateur
             ->add('enabled')
             // ->add('editeur')
         ;
