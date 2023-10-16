e238965c029fdf0fc605c0ba8f0687d6ac3d7d48
Sego <s.henniart.webdev@gmail.com>
2023-10-16 14:25:53 +0200

    slksdfjkgh

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('resume')
+            ->add('contenu')
+            ->add('deletedAt')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('tags')
+            ->add('editeurs')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
