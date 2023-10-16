7bd0dd3b2651b6c80436ec7ae1b65c7baf680083
hugo-michel <orayito62@gmail.com>
2023-10-16 14:27:58 +0200

    function to string tag et editeur

--- src/Entity/Editeur.php
+++ src/Entity/Editeur.php
@@ -104,4 +104,9 @@ class Editeur
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->nom;
+    }
 }
--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -89,4 +89,9 @@ class Tag
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->nom;
+    }
 }
