f704cc83b21967d2d51a12c644495ee8df604678
Sego <s.henniart.webdev@gmail.com>
2023-10-16 13:47:54 +0200

    TestController

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\Editeur;
+use App\Entity\Faq;
 use App\Entity\Tag;
+use App\Entity\User;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Faq;
 
 #[Route('/test')]
 class TestController extends AbstractController
@@ -88,4 +91,145 @@ class TestController extends AbstractController
         ]);
     }
 
+    #[Route('/article', name: 'app_test_article')]
+    public function article(ManagerRegistry $doctrine): Response
+    {
+        $em = $doctrine->getManager();
+        $articleRepository = $em->getRepository(Article::class);
+        $tagRepository = $em->getRepository(Tag::class);
+
+        // NEW : création d'un article
+        $newArticle = new Article();
+        $newArticle->setTitre('Nouveau article');
+        $newArticle->setResume('Nouveau article');
+        $newArticle->setContenu('Contenu de l\'article');
+
+        $newArticle->addTag($tagRepository->find(1));
+
+        $em->persist($newArticle);
+        $em->flush();
+
+        // SHOW : all articles
+        $articles = $articleRepository->findAll();
+        $articlesEditeur = $articleRepository->findByEditeurNotNull();
+
+        $articlesEditeur = 
+
+        // SHOW : one article
+        $article1 = $articleRepository->find(1);
+
+        // UPDATE : edit article
+        $article1->setTitre('TestName');
+        $em->flush();
+
+        // DELETE : delete article
+        $article2 = $articleRepository->find(2);
+        if ($article2) {
+            $em->remove($article2);
+        }
+        $em->flush();
+    
+        return $this->render('test/article.html.twig', [
+            'controller_name' => 'TestController',
+            'newArticle' => $newArticle,
+            'articles' => $articles,
+            'article1' => $article1,
+            'article2' => $article2,
+        ]);
+    }
+
+    #[Route('/editeur', name: 'app_test_editeur')]
+    public function editeur(ManagerRegistry $doctrine): Response {
+
+        $em = $doctrine->getManager();
+        $editeurRepository = $em->getRepository(Editeur::class);
+        // $userRepository = $em->getRepository(User::class);
+
+        // NEW : création d'un editeur
+        $newEditeur = new Editeur();
+        $newEditeur->setNom('Jean-Michel');
+        $newEditeur->setPrenom('La Frite');
+
+        $newLastUser = new User();
+        //* Si cette ligne n'est pas modifiée entre 2 rechargements du twig, elle essaie de créer un nouveau
+        //* user qui a le même email que le précédent : erreur SQLSTATE[23000]: Integrity constraint violation: 1062 Duplicate entry
+        $newLastUser->setEmail($this->$faker->email);
+        $newLastUser->setPassword('123');
+        $newLastUser->setEnabled(true);
+        $newLastUser->setRoles(['ROLE_USER']);
+        $newEditeur->setUser($newLastUser);
+        $em->persist($newEditeur);
+        $em->flush();
+
+        // SHOW : all editeurs
+        $editeurs = $editeurRepository->findAll();
+
+        // SHOW : one editeur
+        $editeur3 = $editeurRepository->find(3);
+
+        // UPDATE : edit editeur
+        $editeur2 = $editeurRepository->find(1);
+        if ($editeur2) {
+            $editeur2->setPrenom('Bidule');
+        }
+        $em->flush();
+
+        // DELETE : delete editeur
+        if ($editeur2) {
+            $em->remove($editeur2);
+        }
+        $em->flush();
+
+
+        return $this->render('test/editeur.html.twig', [
+            'controller_name' => 'TestController',
+            'editeurs' => $editeurs,
+            'editeur3' => $editeur3,
+            'editeur2' => $editeur2,
+            'newEditeur' => $newEditeur,
+        ]);
+    }
+
+    // #[Route('/user', name: 'app_test_user')]
+    // public function user(ManagerRegistry $doctrine): Response {
+        
+    //     $em = $doctrine->getManager();
+    //     $userRepository = $em->getRepository(User::class);
+
+    //     // NEW : nouvel utilisateur
+    //     $newUser = new User();
+    //     //* newUser a le même email que le précédent : erreur SQLSTATE[23000]: Integrity constraint violation: 1062 Duplicate entry
+    //     $newUser->setEmail('foobarressdssses@example.com');
+    //     $newUser->setPassword('123');
+    //     $newUser->setEnabled(true);
+    //     $newUser->setRoles(['ROLE_USER']);
+    //     $em->persist($newUser);
+    //     $em->flush();
+
+    //     // SHOW : all users
+    //     $users = $userRepository->findAll();
+    //     // SHOW : one user
+    //     $user1 = $userRepository->find(1);
+
+    //     // UPDATE : edit user
+    //     $user2 = $userRepository->find(2);
+    //     if ($user2) {
+    //         $user2->setEmail('foobarbaz@example.com');
+    //         $em->flush();
+    //     }
+
+    //     // DELETE : delete user
+    //     if ($user2) {
+    //         $em->remove($user2);
+    //     }
+    //     $em->flush();
+
+    //     return $this->render('test/user.html.twig', [
+    //         'controller_name' => 'TestController',
+    //         'newUser' => $newUser,
+    //         'users' => $users,
+    //         'user1' => $user1,
+    //         'user2' => $user2,
+    //     ]);
+    // }
 }
