b8962d0a1461e5ced32f6eed04e685f2403635ba
hugo-michel <orayito62@gmail.com>
2023-10-16 15:15:13 +0200

    modif form et template tag

--- src/Form/TagType.php
+++ src/Form/TagType.php
@@ -14,9 +14,6 @@ class TagType extends AbstractType
         $builder
             ->add('nom')
             ->add('description')
-            ->add('deletedAt')
-            ->add('createdAt')
-            ->add('updatedAt')
             ->add('articles')
         ;
     }
