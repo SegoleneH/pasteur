2f5d5ab97d4d5eb70440420fa4777e3f521aa5b1
hugo-michel <orayito62@gmail.com>
2023-10-18 10:21:24 +0200

    form fonctionnel user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,16 +3,10 @@
 namespace App\Form;
 
 use App\Entity\User;
-
-// use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-// use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
