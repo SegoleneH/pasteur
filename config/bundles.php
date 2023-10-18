4a3c7af7dc52fc2daaeba65a9961d84573b715ac
Sego <s.henniart.webdev@gmail.com>
2023-10-18 18:27:38 +0200

    ajout liens CDN + menu dans base

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
