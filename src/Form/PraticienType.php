5afdea6cafd735f8c03d4514e56564029c0cc7d8
hugo-michel <orayito62@gmail.com>
2023-10-19 14:32:20 +0200

    debut form prati

--- src/Form/PraticienType.php
+++ src/Form/PraticienType.php
@@ -14,14 +14,43 @@ class PraticienType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('lienRdv')
+            ->add('nom', null, [
+                'label' => 'Nom',
+                'label_attr' => [ 'class' => 'labelForm'],
+                'help' => 'Saisissez le nom du praticien',
+                'help_attr' => [ 'class' => 'helpForm'],
+                'attr' => [
+                    'placeholder' => 'Saisissez le nom du praticien',
+                ],
+            ])
+            ->add('prenom', null, [
+                'label' => 'Prenom',
+                'label_attr' => [ 'class' => 'labelForm'],
+                'help' => 'Saisissez le prenom du praticien',
+                'help_attr' => [ 'class' => 'helpForm'],
+                'attr' => [
+                    'placeholder' => 'Saisissez le prenom du praticien',
+                ],
+            ])
+            ->add('lienRdv', null, [
+                'label' => 'Lien de rdv',
+                'label_attr' => [ 'class' => 'labelForm'],
+                'help' => 'Saisissez le lien de prise de rdv du praticien (doctissimo). Si aucune valeur n\'est saisie, le numéro du standard sera utilisé.',
+                'help_attr' => [ 'class' => 'helpForm'],
+                'attr' => [
+                    'placeholder' => 'Saisissez le lien de rdv du praticien',
+                ],
+            ])
             ->add('metiers', EntityType::class, [
                 'class' => Metier::class,
                 'multiple' => true,
                 'required' => true,
                 'expanded' =>true,
+                'label' => 'Métiers',
+                'label_attr' => [ 'class' => 'labelForm'],
+                'help' => 'Indiquer au moins 1 métier pour le praticien',
+                'help_attr' => [ 'class' => 'helpForm'],
+                'invalid_message' => 'Veuillez indiquer au moins 1 métier',
             ])
         ;
     }
