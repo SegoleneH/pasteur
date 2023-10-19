2a088d65b152a889ff58984d013eec50f475a20b
hugo-michel <orayito62@gmail.com>
2023-10-19 14:03:06 +0200

    modif form label text back metier

--- src/Form/MetierType.php
+++ src/Form/MetierType.php
@@ -12,8 +12,24 @@ class MetierType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('description')
+            ->add('nom', null, [
+                'label' => 'Nom',
+                'label_attr' => [ 'class' => 'labelForm'],
+                'help' => 'Saisissez le nom du métier',
+                'help_attr' => [ 'class' => 'helpForm'],
+                'attr' => [
+                    'placeholder' => 'Saisissez le nom du métier',
+                ],
+            ])
+            ->add('description', null, [
+                'label' => 'Description',
+                'label_attr' => [ 'class' => 'labelForm'],
+                'help' => 'Saisissez la description du métier',
+                'help_attr' => [ 'class' => 'helpForm'],
+                'attr' => [
+                    'placeholder' => 'Saisissez la description du métier',
+                ],
+            ])
         ;
     }
 
