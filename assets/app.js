7e332741966804c7e82532ec94cf2e574097b693
hugo-michel <orayito62@gmail.com>
2023-10-26 09:55:22 +0200

    ajout observer + anim pr btn toTheTop

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,17 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/base.scss';
+
+//observer pr show/hide le btn de scroll top. Reglé si topsection visible à 50%, 
+// apparait en 0.5sec
+const topSection = document.getElementById('topSection');
+const toTheTtop = document.querySelector('.scrollToTheTop');
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        toTheTtop.classList.toggle("hide", entry.isIntersecting)
+    })
+}, {
+    threshold: 0.5,
+})
+
+observer.observe(topSection);
