7dddc13be4a08794c1680967b124b3e3c05ecaf2
hugo-michel <orayito62@gmail.com>
2023-10-27 14:49:42 +0200

    bon aria sur faq + js correspondant

--- assets/script/front.js
+++ assets/script/front.js
@@ -16,21 +16,22 @@ observer.observe(topSection);
 
 const sectionFaq = document.getElementById('sectionFaq');
 const containerFaq = sectionFaq.querySelectorAll('div');
-const expandAllFas = document.getElementById('faqExpand');
+const faqExpand = document.getElementById('faqExpand');
 const faqBtn = document.querySelectorAll('.faqBtn');
 
 //btn expand/close all
 faqExpand.addEventListener('click', () => {
     containerFaq.forEach(element => {
         element.lastElementChild.classList.toggle('reponseHide');
-        element.getAttribute('aria-expanded') === 'false' ? element.setAttribute('aria-expanded', 'true') : element.setAttribute('aria-expanded', 'false')
     })
     faqExpand.textContent = faqExpand.textContent === 'ouvrir toutes les faq' ? 'fermer toutes les faq' : 'ouvrir toutes les faq';
+    faqExpand.getAttribute('aria-expanded') === 'false' ? faqExpand.setAttribute('aria-expanded', 'true') : faqExpand.setAttribute('aria-expanded', 'false');
 })
 //btn pr chaque faq
 faqBtn.forEach(btn => {
     btn.addEventListener('click', () => {
         btn.parentElement.nextElementSibling.classList.toggle('reponseHide');
-        btn.parentElement.parentElement.getAttribute('aria-expanded') === 'false' ? btn.parentElement.parentElement.setAttribute('aria-expanded', 'true') : btn.parentElement.parentElement.setAttribute('aria-expanded', 'false')
+        btn.getAttribute('aria-expanded') === 'false' ? btn.setAttribute('aria-expanded', 'true') : btn.setAttribute('aria-expanded', 'false');
+        btn.parentElement.nextElementSibling.getAttribute('aria-hidden') === 'true' ? btn.parentElement.nextElementSibling.setAttribute('aria-hidden', 'false') : btn.parentElement.nextElementSibling.setAttribute('aria-hidden', 'true');
     })
 })
