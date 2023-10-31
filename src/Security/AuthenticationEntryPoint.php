c86300e3e5607239c7df0f99d4e33ffb437e5202
hugo-michel <orayito62@gmail.com>
2023-10-31 09:53:34 +0100

    erreur 404 pr BO sans loggedin

---
+++ src/Security/AuthenticationEntryPoint.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
+class AuthenticationEntryPoint implements AuthenticationEntryPointInterface
+{
+    private $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null): RedirectResponse
+    {
+        throw new NotFoundHttpException();
+    }
+}
