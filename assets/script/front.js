fc72b234775eb75fb374de2f3db53c69d195c29f
hugo-michel <orayito62@gmail.com>
2023-11-09 11:18:26 +0100

    faq anim + aria fonctionnel new version

--- assets/script/front.js
+++ assets/script/front.js
@@ -47,8 +47,7 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', 
         document.body.classList.toggle('noScroll')
     }
     if ((!(topSection.classList.contains('IsVisible'))) && (toTheTtop.classList.contains('hide')) ) {
-        
-            toTheTtop.classList.remove("hide")
+        toTheTtop.classList.remove("hide")
      } else {
             return
         }  
@@ -56,40 +55,6 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', 
 
 //accordeon pour la section faq
 
-// const sectionFaq = document.getElementById('sectionFaq');
-
-// if (sectionFaq) {
-
-// const containerFaq = sectionFaq.querySelectorAll('div');
-// const faqExpand = document.getElementById('faqExpand');
-// const faqBtn = document.querySelectorAll('.faqBtn');
-
-// //btn expand/close all
-// faqExpand.addEventListener('click', () => {
-//     containerFaq.forEach(element => {
-//         if (faqExpand.textContent === 'Ouvrir toutes les réponses') {
-//             element.firstElementChild.lastElementChild.textContent = 'Fermer';
-//             element.lastElementChild.setAttribute('aria-hidden', 'false');
-//             element.lastElementChild.classList.remove('reponseHide');
-//         } else {
-//             element.firstElementChild.lastElementChild.textContent = 'Ouvrir';
-//             element.lastElementChild.setAttribute('aria-hidden', 'true');
-//             element.lastElementChild.classList.add('reponseHide');
-//         }
-//     })
-//     faqExpand.textContent = faqExpand.textContent === 'Ouvrir toutes les réponses' ? 'Fermer toutes les réponses' : 'Ouvrir toutes les réponses';
-//     faqExpand.getAttribute('aria-expanded') === 'false' ? faqExpand.setAttribute('aria-expanded', 'true') : faqExpand.setAttribute('aria-expanded', 'false');
-// })
-// //btn pr chaque faq
-// faqBtn.forEach(btn => {
-//     btn.addEventListener('click', () => {
-//         btn.parentElement.nextElementSibling.classList.toggle('reponseHide');
-//         btn.getAttribute('aria-expanded') === 'false' ? btn.setAttribute('aria-expanded', 'true') : btn.setAttribute('aria-expanded', 'false');
-//         btn.textContent === 'Ouvrir' ? btn.textContent = 'Fermer' : btn.textContent = 'Ouvrir';
-//         btn.parentElement.nextElementSibling.getAttribute('aria-hidden') === 'true' ? btn.parentElement.nextElementSibling.setAttribute('aria-hidden', 'false') : btn.parentElement.nextElementSibling.setAttribute('aria-hidden', 'true');
-//     })
-// })
-// }
 
 const sectionFaq = document.getElementById('sectionFaq');
 
@@ -104,13 +69,19 @@ const spans = document.querySelectorAll('.faqSpan');
 faqExpand.addEventListener('click', () => {
     containerFaq.forEach(element => {
         if (faqExpand.textContent === 'Ouvrir toutes les réponses') {
-            // element.firstElementChild.lastElementChild.textContent = 'Fermer';
+            element.setAttribute('aria-expanded', 'true');
             element.lastElementChild.setAttribute('aria-hidden', 'false');
             element.lastElementChild.classList.remove('reponseHide');
+            if (!(element.firstElementChild.lastElementChild.classList.contains('active'))) {
+                element.firstElementChild.lastElementChild.classList.toggle('active')
+            }
         } else {
-            // element.firstElementChild.lastElementChild.textContent = 'Ouvrir';
+            element.setAttribute('aria-expanded', 'false');
             element.lastElementChild.setAttribute('aria-hidden', 'true');
             element.lastElementChild.classList.add('reponseHide');
+            if ((element.firstElementChild.lastElementChild.classList.contains('active'))) {
+                element.firstElementChild.lastElementChild.classList.toggle('active')
+            }
         }
     })
     faqExpand.textContent = faqExpand.textContent === 'Ouvrir toutes les réponses' ? 'Fermer toutes les réponses' : 'Ouvrir toutes les réponses';
@@ -122,7 +93,6 @@ containerFaq.forEach(ctn => {
         ctn.lastElementChild.classList.toggle('reponseHide');
         ctn.firstElementChild.lastElementChild.classList.toggle('active')
         ctn.getAttribute('aria-expanded') === 'false' ? ctn.setAttribute('aria-expanded', 'true') : ctn.setAttribute('aria-expanded', 'false');
-        // btn.textContent === 'Ouvrir' ? btn.textContent = 'Fermer' : btn.textContent = 'Ouvrir';
         ctn.lastElementChild.getAttribute('aria-hidden') === 'true' ? ctn.lastElementChild.setAttribute('aria-hidden', 'false') : ctn.lastElementChild.setAttribute('aria-hidden', 'true');
     })
 })
